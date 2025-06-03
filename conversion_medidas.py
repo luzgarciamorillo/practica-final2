70ae718ddeda77fe2d255c69a073661e979557b1
Luz García Morillo <luzgarciamorillo@MacBook-Pro-de-Luz.local>
2025-06-03 15:11:32 -0300

    Hicimos un código para convertir cms a pulgadas

---
+++ conversion_medidas.py
@@ -0,0 +1,12 @@
+# ----------------------------------------
+# PRÁCTICA FINAL 2: CONVERSIÓN DE MEDIDAS
+# ----------------------------------------
+
+# Paso 1: Solicitar al usuario las medidas del mueble en Compañía
+medida_en_cm = float(input("Por favor, ingresar las medidas de la pieza del mueble (en cm): "))
+
+# Paso 2: Convertir las medidas de centímetros a pulgadas
+medida_en_pulgadas = medida_en_cm / 2.54
+
+# Paso 3: Mostrar las medidas convertidas en pulgadas al usuario
+print("Las medidas en pulgadas de la pieza ingresadas son: ", medida_en_pulgadas)
